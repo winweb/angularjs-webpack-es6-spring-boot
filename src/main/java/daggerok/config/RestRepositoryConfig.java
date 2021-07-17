b9b90d1e37bd1eb60b6e3e54b50b26700f807088
winweb <theerasun@gmail.com>
2021-07-17 23:54:54 +0700

    upgrade spring boot version 2.5. and make sample test case.

--- src/main/java/daggerok/config/RestRepositoryConfig.java
+++ src/main/java/daggerok/config/RestRepositoryConfig.java
@@ -3,12 +3,10 @@ package daggerok.config;
 import daggerok.domain.user.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @Configuration
-public class RestRepositoryConfig extends RepositoryRestConfigurerAdapter {
-
-    @Override
+public class RestRepositoryConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(User.class);
     }
--- src/test/java/daggerok/Ng15AppTests.java
+++ src/test/java/daggerok/Ng15AppTests.java
@@ -1,14 +1,22 @@
 package daggerok;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Ng15AppTests {
 
     @Test
-    public void contextLoads() {}
+    public void contextLoads() {
+        assertTrue("foo".equals("foo"));
+    }
+
+    @Test
+    void test(){
+        assertThat("foo").isEqualTo("foo");
+    }
 }
